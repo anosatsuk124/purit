357dc21e2ce7d8836ebc502a039884333c3ea3bd
Satsuki Akiba <anosatsuk124@gmail.com>
2021-10-22 13:39:05 +0900

    Add init command

---
+++ src/Cmd/Init.purs
@@ -0,0 +1,29 @@
+module Cmd.Init where
+
+import Effect.Console
+import Prelude
+
+import Data.Unit (Unit)
+import Effect (Effect)
+import Effect.Exception (throwException)
+import Effect.Unsafe (unsafePerformEffect)
+import Node.FS.Sync as FS
+import Node.Encoding as NE
+
+
+init :: Effect Unit
+init = do
+    if unsafePerformEffect (FS.exists(gitdir))
+      then log "The git repository already exists."
+    else do
+      FS.mkdir gitdir
+      FS.mkdir $ gitdir <> "branches"
+      FS.mkdir $ gitdir <> "objects"
+      FS.mkdir $ gitdir <> "refs"
+      FS.mkdir $ gitdir <> "refs" <> "tags"
+      FS.mkdir $ gitdir <> "refs" <> "heads"
+      FS.writeTextFile NE.UTF8 (gitdir <> "HEAD") "ref: refs/heads/master\n"
+      FS.writeTextFile NE.UTF8 (gitdir <> "description") "Unnamed repository; edit this file 'description' to name the repository.\n"
+      
+    where
+          gitdir = ".git/"
--- src/Main.purs
+++ src/Main.purs
@@ -3,8 +3,17 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Console
+import Data.Array
+import Data.Maybe
+import JS.Argv (argv)
+import Cmd.Init (init)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  case (args !! 0) of
+       Just "init" -> init
+       Just (_) -> log "1"
+       Nothing -> log "0"
+  where
+        args = drop 2 argv
