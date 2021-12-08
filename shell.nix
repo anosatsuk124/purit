d5d362861e292fe953a4411d25b77ad7278cfb08
Satsuki Akiba <anosatsuk124@gmail.com>
2021-12-08 13:46:44 +0000

    remove purescript components and add nix-shell file

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  easy-ps = import (pkgs.fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "master";
+    sha256 = "1gva113kyygjhn9i92vg6cyj15vhyfhq7haq51cvp4xdz4j0q4xn";
+  }) { inherit pkgs; };
+in
+pkgs.mkShell {
+  buildInputs = [
+    easy-ps.purs-0_14_5
+    easy-ps.spago
+    pkgs.nodejs-16_x
+  ];
+}
